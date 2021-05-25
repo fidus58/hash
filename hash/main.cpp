20480eafffdec02ddd8f5649cd48375afaecdb4c
fidus58 <ahrens@informatik.hu-berlin.de>
2021-05-25 13:57:13 +0200

    strange hash distribution

--- hash/main.cpp
+++ hash/main.cpp
@@ -7,9 +7,43 @@
 //
 
 #include <iostream>
+#include <unordered_set>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+class X {
+public:
+    X* i_= 0;
+
+    X():i_{this} {}
+    //X(const X& other):i_{other.i_}{std::cout<<"copy\n";}
+    friend std::ostream& operator<<
+    (std::ostream& o, const X& x)
+    {return o<<x.i_;}
+    friend struct std::hash<X>;
+    bool operator==(const X&) const
+    {return false;}
+};
+
+template<>
+struct std::hash<X> {
+      size_t operator()(const X& x) const {
+        return hash<const X*>()(&x);
+      }
+};
+
+int main() {
+    std::unordered_multiset<X> xs;
+
+    for (int i=0; i<10; ++i) { X x{}; xs.insert(x); }
+    for (int i=0; i<10; ++i) { X x{}; xs.insert(x); }
+ 
+    for (auto& x: xs) {std::cout<<&x<<" ";}
+    //for (auto& x: xs) {std::cout<<x<<" ";}
+    std::cout<<"\n";
+    for (size_t bc=0; bc<xs.bucket_count(); ++bc)
+        std::cout<<"bucket["<<bc<<"]: "<< xs.bucket_size(bc) <<"\n";
+    std::cout<< xs.load_factor() << "\n";
+    xs.rehash(10);
+    for (size_t bc=0; bc<xs.bucket_count(); ++bc)
+        std::cout<<"bucket["<<bc<<"]: "<< xs.bucket_size(bc) <<"\n";
+    std::cout<< xs.load_factor() << "\n";
 }
